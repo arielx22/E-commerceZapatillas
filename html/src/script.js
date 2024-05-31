c6857c18d782fa784492b70c054a3b67c21f0002
nabunave <toto_saynomore@hotmail.com>
2024-05-31 11:09:02 -0300

    responsive en nav y footer
    se corrigieron medias querys en el nav, agregado menu burguer, falta corregir el hover y espaciado en menu en tamaño original

--- html/src/script.js
+++ html/src/script.js
@@ -1,4 +1,12 @@
 /* script.js */
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
 let slideIndex = 0;
 
 function showSlides() {
